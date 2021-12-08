195df8fa82c71d514d58d877ef5ff8b8fe994785
Andrew <andre.cerio91@gmail.com>
2021-12-08 23:59:25 +0100

    Resolve Day one puzzle pt.2

--- advent_of_code/lib/ao_c.ex
+++ advent_of_code/lib/ao_c.ex
@@ -1,5 +1,6 @@
 defmodule AoC do
   def main do
-    DayOne.Pt1.resolve_puzzle
+    #DayOne.Pt1.resolve_puzzle
+    DayOne.Pt2.resolve_puzzle
   end
 end
--- advent_of_code/lib/ao_c/day_one.ex
+++ advent_of_code/lib/ao_c/day_one.ex
@@ -1,14 +1,9 @@
 defmodule DayOne do
   defmodule Pt1 do
     def resolve_puzzle do
-      input_path = Path.join(File.cwd!, "media/input01.txt")
-      read_input(input_path)
-      |> gen_stat
-    end
-
-    def read_input(path) do
-      {:ok, data} = File.read(path)
+      {:ok, data} = File.read(Path.join(~w(#{File.cwd!} media/input01.txt)))
       data
+      |> gen_stat
     end
 
     def gen_stat(data) do
@@ -26,7 +21,23 @@ defmodule DayOne do
 
   defmodule Pt2 do
     def resolve_puzzle do
+      {:ok, data} = File.read(Path.join(~w(#{File.cwd!} media/input01.txt)))
+      data
+      |> gen_stat
+    end
 
+    def gen_stat(data) do
+      data
+      |> String.split("\n")
+      |> Enum.filter(&(&1 != ""))
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.chunk_every(3, 1, :discard)
+      |> Enum.map(fn [x, y, z] -> x + y + z end)
+      |> Enum.chunk_every(2, 1, :discard)
+      |> Enum.map(fn [x, y] -> y - x end)
+      |> Enum.reduce(0, fn (x, acc) ->
+        if x > 0, do: 1 + acc, else: acc
+      end)
     end
   end
 end
