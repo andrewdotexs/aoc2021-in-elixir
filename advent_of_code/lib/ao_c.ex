07d3b6af294b98cf5075152df228b7153dea4f1d
andrewdotexs <cerianiandrea.91@gmail.com>
2021-12-10 00:01:10 +0100

    Resolve Day two puzzle pt.1

--- advent_of_code/lib/ao_c.ex
+++ advent_of_code/lib/ao_c.ex
@@ -1,6 +1,7 @@
 defmodule AoC do
   def main do
     #DayOne.Pt1.resolve_puzzle
-    DayOne.Pt2.resolve_puzzle
+    #DayOne.Pt2.resolve_puzzle
+    DayTwo.Pt1.resolve_puzzle
   end
 end
---
+++ advent_of_code/lib/ao_c/day_two.ex
@@ -0,0 +1,35 @@
+defmodule DayTwo do
+  defmodule Result do
+    defstruct [f_tot: 0, d_tot: 0]
+  end
+
+  defmodule Pt1 do
+    def resolve_puzzle do
+      {:ok, data} = File.read(Path.join(~w(#{File.cwd!} media/input02.txt)))
+      data
+      |> parse_input
+    end
+
+    def parse_input(data) do
+      magn = %{"forward" => 1, "down" => 1, "up" => -1}
+
+      res = data
+      |> String.split("\n")
+      |> Enum.filter(&(&1 != ""))
+      |> Enum.reduce(%DayTwo.Result{}, fn x, acc ->
+        [command, units] = String.split(x, " ")
+        command_magn = magn[command]
+        value = String.to_integer(units)
+        #IO.puts "command: #{command} [#{command_magn}] <- #{value}"
+        case command do
+          "forward" ->
+            %DayTwo.Result{acc | f_tot: acc.f_tot + (value * command_magn)}
+          c when c == "down" or c == "up" ->
+            %DayTwo.Result{acc | d_tot: acc.d_tot + (value * command_magn)}
+        end
+      end)
+
+      res.f_tot * res.d_tot
+    end
+  end
+end
